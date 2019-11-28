08057132a8820a3d8e343a9018e2f72ae1226245
porfirovskiy <porfirovskiy@gmail.com>
2019-11-28 22:17:30 +0200

    finish with image update form

--- backend/models/ImageUpdateForm.php
+++ backend/models/ImageUpdateForm.php
@@ -3,6 +3,9 @@ namespace backend\models;
 
 use yii\base\Model;
 use backend\models\Images;
+use frontend\models\ImagesTags;
+use frontend\models\Tags;
+use frontend\models\Descriptions;
 
 class ImageUpdateForm extends Model
 {
@@ -24,32 +27,31 @@ class ImageUpdateForm extends Model
     
     public function update(int $id): bool
     {
-        //update image
-        $image = Images::find()->where(['id' => $id]);
-        $image->name = $this->name;
-        $image->source = $this->source;
-        $image->translit_name = $this->translit_name;
-        $image->update();
-        //update tags
-        
-        //update description
-        if (isset($this->description) && !empty($this->description)) {
-            $descModel = Descriptions::find()->where(['image_id' => $id]);;
-            $descModel->text = $this->description;
-            $descModel->update();
-        }
-        
-        
-        echo '<pre>';var_dump($model);die();
-        $this->hash = sha1_file($this->imageFile->tempName);
-        if ($this->validate()) {
-            $this->imageName = $this->getUniqName();
-            $this->imagePath = $this->getImageDir($this->dir). $this->imageName . '.' . $this->imageFile->extension;
-            $this->imageFile->saveAs($this->imagePath);
+        $transaction = \Yii::$app->db->beginTransaction();
+        try
+        {
+            //update image
+            $image = Images::findOne(['id' => $id]);
+            $image->name = $this->name;
+            $image->source = $this->source;
+            $image->translit_name = $this->translit_name;
+            $image->update();
+            //update tags
+            ImagesTags::deleteAll(['image_id' => $id]);
+            $tagsModel = new Tags();
+            $tagsModel->saveImageTags($this->tags, $id);
+            //IF ONLY IMAGE CHANGE - UPDATE!!!!
+            //update description
+            if (isset($this->description) && !empty($this->description)) {
+                $descModel = Descriptions::find()->where(['image_id' => $id])->one();
+                $descModel->text = $this->description;
+                $descModel->update();
+            }
+            $transaction->commit();
             return true;
-        } else {
-            \Yii::$app->session->setFlash('error', 'Error -> ' . serialize($this->getErrors()));
-            return false;
+        } catch(Exception $e) {
+           $transaction->rollBack();
+           return false;
         }
     }
     
