849e62415f5e85925cdfafba5d35c4c2b69adb06
porfirovskiy <porfirovskiy@gmail.com>
2019-12-26 23:00:18 +0200

    fix 2 fields

--- console/migrations/m191121_205718_add_admin_user.php
+++ console/migrations/m191121_205718_add_admin_user.php
@@ -17,7 +17,9 @@ class m191121_205718_add_admin_user extends Migration
             'auth_key' => 'i92wh4jp8m7KinJlzHIZiyxmprpGdwQC',
             'password_hash' => '$2y$13$mnvwFfLvW.swBLjXN5INYOnFSn7tnFWdIoAp3urHrc/0lfPETzEtq',
             'email' => 'img2user@gmail.com',
-            'status' => 10
+            'status' => 10,
+            'created_at' => 0,
+            'updated_at' => 0
         ]);
     }
 
