630685ef4d249d1284312b99ba18f6f72fa1d789
porfirovskiy <porfirovskiy@gmail.com>
2020-02-09 20:43:05 +0200

    add GA script to HEAD section

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -17,6 +17,16 @@ AppAsset::register($this);
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
 <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-151965973-2"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-151965973-2');
+    </script>
+
     <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
